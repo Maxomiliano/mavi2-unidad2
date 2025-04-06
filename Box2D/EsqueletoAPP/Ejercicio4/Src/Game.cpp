9bcaf26a7ec630fdaa91a703e08b44542b1f8b9d
Maxomiliano <max.nebula1@gmail.com>
2025-04-05 21:21:39 -0300

    Ejercicio 4

--- Box2D/EsqueletoAPP/Ejercicio4/Src/Game.cpp
+++ Box2D/EsqueletoAPP/Ejercicio4/Src/Game.cpp
@@ -5,37 +5,37 @@
 // Constructor de la clase Game
 Game::Game(int ancho, int alto, std::string titulo)
 {
-    // Inicializaci�n de la ventana y configuraci�n de propiedades
-    wnd = new RenderWindow(VideoMode(ancho, alto), titulo);
-    wnd->setVisible(true);
-    fps = 60;
-    wnd->setFramerateLimit(fps);
-    frameTime = 1.0f / fps;
-    SetZoom(); // Configuraci�n de la vista del juego
-    InitPhysics(); // Inicializaci�n del motor de f�sica
-    controlBody->ApplyLinearImpulse(b2Vec2(10000, 10), b2Vec2(50, 50), true);
+	// Inicializaci�n de la ventana y configuraci�n de propiedades
+	wnd = new RenderWindow(VideoMode(ancho, alto), titulo);
+	wnd->setVisible(true);
+	fps = 60;
+	wnd->setFramerateLimit(fps);
+	frameTime = 1.0f / fps;
+	SetZoom(); // Configuraci�n de la vista del juego
+	InitPhysics(); // Inicializaci�n del motor de f�sica
+	//controlBody->ApplyLinearImpulse(b2Vec2(10000, 10), b2Vec2(50, 50), true);
 }
 
 // Bucle principal del juego
 void Game::Loop()
 {
-    while (wnd->isOpen())
-    {
-        wnd->clear(clearColor); // Limpiar la ventana
-        DoEvents(); // Procesar eventos de entrada
-        CheckCollitions(); // Comprobar colisiones
-        UpdatePhysics(); // Actualizar la simulaci�n f�sica
-        DrawGame(); // Dibujar el juego
-        wnd->display(); // Mostrar la ventana
-    }
+	while (wnd->isOpen())
+	{
+		wnd->clear(clearColor); // Limpiar la ventana
+		DoEvents(); // Procesar eventos de entrada
+		CheckCollitions(); // Comprobar colisiones
+		UpdatePhysics(); // Actualizar la simulaci�n f�sica
+		DrawGame(); // Dibujar el juego
+		wnd->display(); // Mostrar la ventana
+	}
 }
 
 // Actualizaci�n de la simulaci�n f�sica
 void Game::UpdatePhysics()
 {
-    phyWorld->Step(frameTime, 8, 8); // Simular el mundo f�sico
-    phyWorld->ClearForces(); // Limpiar las fuerzas aplicadas a los cuerpos
-    phyWorld->DebugDraw(); // Dibujar el mundo f�sico para depuraci�n
+	phyWorld->Step(frameTime, 8, 8); // Simular el mundo f�sico
+	phyWorld->ClearForces(); // Limpiar las fuerzas aplicadas a los cuerpos
+	phyWorld->DebugDraw(); // Dibujar el mundo f�sico para depuraci�n
 }
 
 // Dibujo de los elementos del juego
@@ -44,71 +44,75 @@ void Game::DrawGame() {}
 // Procesamiento de eventos de entrada
 void Game::DoEvents()
 {
-    Event evt;
-    while (wnd->pollEvent(evt))
-    {
-        switch (evt.type)
-        {
-        case Event::Closed:
-            wnd->close(); // Cerrar la ventana si se presiona el bot�n de cerrar
-            break;
-        }
-    }
+	Event evt;
+	while (wnd->pollEvent(evt))
+	{
+		switch (evt.type)
+		{
+		case Event::Closed:
+			wnd->close(); // Cerrar la ventana si se presiona el bot�n de cerrar
+			break;
+		}
+	}
+
+	controlBody->SetAwake(true);
+	if (Keyboard::isKeyPressed(Keyboard::Left))
+	{
+		controlBody->ApplyForce(b2Vec2(-200, 0), controlBody->GetWorldCenter(), true);
+	}
+	if (Keyboard::isKeyPressed(Keyboard::Right))
+	{
+		controlBody->ApplyForce(b2Vec2(200, 0), controlBody->GetWorldCenter(), true);
+	}
 }
 
 // Comprobaci�n de colisiones (a implementar m�s adelante)
 void Game::CheckCollitions()
 {
-    // Implementaci�n de la comprobaci�n de colisiones
+	// Implementaci�n de la comprobaci�n de colisiones
 }
 
 // Configuraci�n de la vista del juego
 void Game::SetZoom()
 {
-    View camara;
-    // Posicionamiento y tama�o de la vista
-    camara.setSize(100, 100);
-    camara.setCenter(50, 50);
-    wnd->setView(camara); // Asignar la vista a la ventana
+	View camara;
+	// Posicionamiento y tama�o de la vista
+	camara.setSize(100, 100);
+	camara.setCenter(50, 50);
+	wnd->setView(camara); // Asignar la vista a la ventana
 }
 
 // Inicializaci�n del motor de f�sica y los cuerpos del mundo f�sico
 void Game::InitPhysics()
 {
-    // Inicializar el mundo f�sico con la gravedad por defecto
-    phyWorld = new b2World(b2Vec2(0.0f, 9.8f));
+	// Inicializar el mundo f�sico con la gravedad por defecto
+	phyWorld = new b2World(b2Vec2(0.0f, 9.8f));
 
-    // Crear un renderer de debug para visualizar el mundo f�sico
-    debugRender = new SFMLRenderer(wnd);
-    debugRender->SetFlags(UINT_MAX);
-    phyWorld->SetDebugDraw(debugRender);
+	// Crear un renderer de debug para visualizar el mundo f�sico
+	debugRender = new SFMLRenderer(wnd);
+	debugRender->SetFlags(UINT_MAX);
+	phyWorld->SetDebugDraw(debugRender);
 
-    // Crear el suelo y las paredes est�ticas del mundo f�sico
-    b2Body* groundBody = Box2DHelper::CreateRectangularStaticBody(phyWorld, 100, 10);
-    groundBody->SetTransform(b2Vec2(50.0f, 100.0f), 0.0f);
+	// Crear el suelo y las paredes est�ticas del mundo f�sico
+	//b2Body* groundBody = Box2DHelper::CreateRectangularStaticBody(phyWorld, 100, 10);
+	//groundBody->SetTransform(b2Vec2(50.0f, 100.0f), 0.0f);
+	b2Body* groundBody = Box2DHelper::CreateStaticBody(phyWorld);
+	b2FixtureDef box = Box2DHelper::CreateRectangularFixtureDef(100, 10, 0.0f, 0.03f, 0.0f);
+	groundBody->CreateFixture(&box);
+	groundBody->SetTransform(b2Vec2(50.0f, 100.0f), 0.0f);
 
 	b2Body* ceilingBody = Box2DHelper::CreateRectangularStaticBody(phyWorld, 100, 10);
 	ceilingBody->SetTransform(b2Vec2(50.0f, 0.0f), 0.0f);
 
-    b2Body* leftWallBody = Box2DHelper::CreateRectangularStaticBody(phyWorld, 10, 100);
-    leftWallBody->SetTransform(b2Vec2(0.0f, 50.0f), 0.0f);
+	b2Body* leftWallBody = Box2DHelper::CreateRectangularStaticBody(phyWorld, 10, 100);
+	leftWallBody->SetTransform(b2Vec2(0.0f, 50.0f), 0.0f);
 
-    b2Body* rightWallBody = Box2DHelper::CreateRectangularStaticBody(phyWorld, 10, 100);
-    rightWallBody->SetTransform(b2Vec2(100.0f, 50.0f), 0.0f);
+	b2Body* rightWallBody = Box2DHelper::CreateRectangularStaticBody(phyWorld, 10, 100);
+	rightWallBody->SetTransform(b2Vec2(100.0f, 50.0f), 0.0f);
 
-    b2Body* squareObstacleBody = Box2DHelper::CreateRectangularStaticBody(phyWorld, 10, 10);
-	squareObstacleBody->SetTransform(b2Vec2(80.0f, 50.0f), 0.0f);
-
-	b2Body* triangleObstacleBody = Box2DHelper::CreateTriangularStaticBody(phyWorld, b2Vec2(5,5), 10);
-	triangleObstacleBody->SetTransform(b2Vec2(20.0f, 30.0f), 0.0f);
-
-	b2Body* circleObstacleBody = Box2DHelper::CreateCircularStaticBody(phyWorld, 5);
-	circleObstacleBody->SetTransform(b2Vec2(40.0f, 70.0f), 0.0f);
-
-
-    // Crear un c�rculo que se controlar� con el teclado
-    controlBody = Box2DHelper::CreateCircularDynamicBody(phyWorld, 5, 1.0f, 0.5, 1.0f);
-    controlBody->SetTransform(b2Vec2(50.0f, 50.0f), 0.0f);
+	// Crear un c�rculo que se controlar� con el teclado
+	controlBody = Box2DHelper::CreateRectangularDynamicBody(phyWorld, 10, 10, 1, 0.03f, 0.1f);
+	controlBody->SetTransform(b2Vec2(50.0f, 90.0f), 0.0f);
 }
 
 // Destructor de la clase
